451e760883e1227f59d229c484a079ca285c30bc
rope-park <kelly6789@naver.com>
2025-02-16 17:32:47 +0900

    Feat: alice1, alice2, alice3의 연속적인 animation 처리 기능 추가
    Promising Chaining 사용

--- projects/sequencing-animation/main.js
+++ projects/sequencing-animation/main.js
@@ -15,38 +15,25 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-// alice1에 애니메이션 효과 적용 - 1. callback 함수를 이용한 방법
-function aliceTumbling1st(callback) {
-    alice1.animate(aliceTumbling, aliceTiming);
-    // 애니메이션이 끝나면 callback 함수 호출
-    setTimeout(() => {
-        console.log("1st animation done!");
-        callback();
-    }, 1000);
-}
+// alice에 애니메이션 효과 적용 - 2. Promise Chaining 활용
+// Promise를 반환하는 함수를 정의하고, then 메소드를 활용하여 순차적으로 애니메이션을 실행
+const alicePromise = (element) => {
+    return element.animate(aliceTumbling, aliceTiming).finished;
+};
 
-function aliceTumbling2nd(callback) {
-    alice2.animate(aliceTumbling, aliceTiming);
-    // 애니메션이 끝나면 callback 함수 호출
-    setTimeout(() => {
-        console.log("2nd animation done!");
-        callback();
-    }, 1000);
-}
-
-function aliceTumbling3rd(callback) {
-    alice3.animate(aliceTumbling, aliceTiming);
-    setTimeout(() => {
-        console.log("3rd animation done!");
-        callback();
-    }, 1000);
-}
-
-// callback hell 발생
-aliceTumbling1st(() => {
-    aliceTumbling2nd(() => {
-        aliceTumbling3rd(() => {
-            console.log("All animations are done!");
-        });
+// element 애니메이션을 Promise로 반환하는 함수
+alicePromise(alice1)
+    .then(() => {
+        console.log("첫 번째 alice 애니메이션 완료");
+        return alicePromise(alice2);
+    })
+    .then(() => {
+        console.log("두 번째 alice 애니메이션 완료");
+        return alicePromise(alice3);
+    })
+    .then(() => {
+        console.log("세 번째 alice 애니메이션 완료");
+    })
+    .catch((error) => {
+        console.error("오류 발생: ", error);
     });
-});
