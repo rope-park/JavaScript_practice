a28cc7c9ae201a8b080a9b6b3de76c8ba5a354e9
rope-park <kelly6789@naver.com>
2025-02-16 16:39:58 +0900

    Feat: alice1, alice2, alice3의 연속적인 animation 처리 기능 추가
    callback 사용

--- projects/sequencing-animation/main.js
+++ projects/sequencing-animation/main.js
@@ -15,5 +15,38 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-// alice1에 애니메이션 효과 적용
-alice1.animate(aliceTumbling, aliceTiming);
+// alice1에 애니메이션 효과 적용 - 1. callback 함수를 이용한 방법
+function aliceTumbling1st(callback) {
+    alice1.animate(aliceTumbling, aliceTiming);
+    // 애니메이션이 끝나면 callback 함수 호출
+    setTimeout(() => {
+        console.log("1st animation done!");
+        callback();
+    }, 1000);
+}
+
+function aliceTumbling2nd(callback) {
+    alice2.animate(aliceTumbling, aliceTiming);
+    // 애니메션이 끝나면 callback 함수 호출
+    setTimeout(() => {
+        console.log("2nd animation done!");
+        callback();
+    }, 1000);
+}
+
+function aliceTumbling3rd(callback) {
+    alice3.animate(aliceTumbling, aliceTiming);
+    setTimeout(() => {
+        console.log("3rd animation done!");
+        callback();
+    }, 1000);
+}
+
+// callback hell 발생
+aliceTumbling1st(() => {
+    aliceTumbling2nd(() => {
+        aliceTumbling3rd(() => {
+            console.log("All animations are done!");
+        });
+    });
+});
